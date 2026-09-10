df69e12cafa8b1884c5f33d7449225b67cbbd1f7
Ahmadday <88631509+Ahmadday@users.noreply.github.com>
2026-09-10 14:21:16 -0400

    Fix CSS import path in PathFindingVisualizer.jsx

--- src/pathFindingVisualizer/PathFindingVisualizer.jsx
+++ src/pathFindingVisualizer/PathFindingVisualizer.jsx
@@ -4,7 +4,7 @@ import { bfs } from './algorithms/bfs';
 import { dfs } from './algorithms/dfs';
 import { astar } from './algorithms/astar';
 import { dijkstra, getNodesInShortestPathOrder } from './algorithms/dijkstra';
-import './PathFindingVisualizer.css';
+import './pathFindingVisualizer.css';
 
 const INITIAL_START = { row: 10, col: 15 };
 const INITIAL_FINISH = { row: 10, col: 35 };
